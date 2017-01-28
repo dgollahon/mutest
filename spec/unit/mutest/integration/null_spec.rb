a6dca1451c1306c03326480ef867ccba07a94dc9
Daniel Gollahon <daniel@blockscore.com>
2017-01-27 21:20:39 -0800

    Fix RSpec/MultipleDescribes offenses

---
+++ spec/unit/mutest/integration/null_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Mutest::Integration::Null do
+  let(:object) { described_class.new(Mutest::Config::DEFAULT) }
+
+  describe '#all_tests' do
+    subject { object.all_tests }
+
+    it { is_expected.to eql([]) }
+
+    it_should_behave_like 'an idempotent method'
+  end
+
+  describe '#call' do
+    subject { object.call(tests) }
+
+    let(:tests) { instance_double(Array) }
+
+    it 'returns test result' do
+      is_expected.to eql(
+        Mutest::Result::Test.new(
+          output:  '',
+          passed:  true,
+          runtime: 0.0,
+          tests:   tests
+        )
+      )
+    end
+  end
+end
--- spec/unit/mutest/integration_spec.rb
+++ spec/unit/mutest/integration_spec.rb
@@ -24,32 +24,3 @@ RSpec.describe Mutest::Integration do
     it { is_expected.to be(Mutest::Integration::Null) }
   end
 end
-
-RSpec.describe Mutest::Integration::Null do
-  let(:object) { described_class.new(Mutest::Config::DEFAULT) }
-
-  describe '#all_tests' do
-    subject { object.all_tests }
-
-    it { is_expected.to eql([]) }
-
-    it_should_behave_like 'an idempotent method'
-  end
-
-  describe '#call' do
-    subject { object.call(tests) }
-
-    let(:tests) { instance_double(Array) }
-
-    it 'returns test result' do
-      is_expected.to eql(
-        Mutest::Result::Test.new(
-          output:  '',
-          passed:  true,
-          runtime: 0.0,
-          tests:   tests
-        )
-      )
-    end
-  end
-end
