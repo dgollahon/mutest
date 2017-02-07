f4b8480a7fc9a1e28ec33479d1314ca11e959e43
Daniel Gollahon <daniel@blockscore.com>
2017-02-06 23:21:07 -0800

    Fix Arguments#hintworthy_node? mutations

--- meta/kwoptarg.rb
+++ meta/kwoptarg.rb
@@ -1,11 +1,11 @@
 Mutest::Meta::Example.add :kwarg do
-  source 'def foo(bar: baz); end'
+  source 'def foo(bar: {}); end'
 
   mutation 'def foo; end'
-  mutation 'def foo(bar: baz); raise; end'
-  mutation 'def foo(bar: baz); super; end'
+  mutation 'def foo(bar: {}); raise; end'
+  mutation 'def foo(bar: {}); super; end'
   mutation 'def foo(bar: nil); end'
   mutation 'def foo(bar: self); end'
   mutation 'def foo(bar:); end'
-  mutation 'def foo(_bar: baz); end'
+  mutation 'def foo(_bar: {}); end'
 end
